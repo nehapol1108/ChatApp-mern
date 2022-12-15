f19442cf4ead32a040f485897d0727356c1612f8
nehapol <nehapol40@gmail.com>
2022-12-15 22:46:07 +0530

    some

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -12,7 +12,7 @@ import ScrollableChat from './ScrollableChat';
 import io from "socket.io-client"
 import Lottie from 'react-lottie'
 import animationData from "../animations/typing.json";
-const ENDPOINT="http://localhost:5000";
+const ENDPOINT="https://mern-chat-website.onrender.com";
 var socket,selectedChatCompare;
 const SingleChat = ({fetchAgain,setFetchAgain}) => {
     const {user,selectedChat,setSelectedChat,notification,setNotification} =ChatState();
